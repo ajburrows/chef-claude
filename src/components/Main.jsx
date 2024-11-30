7e074565468dc3e3a7ddf72bd27d01b32b1ebb9e
Alex Burrows <burrows.alexj@gmail.com>
2024-11-30 18:58:10 -0500

    Only render ingredients section if ingredients are entered

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -25,17 +25,20 @@ export default function Main(){
                 />
                 <button>Add ingredient</button>
             </form>
-            <section className="ingredient-section">
-                <h2 className="ingredient-list-title">Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{ingredients_list_items}</ul>
-                <div className="get-recipe-container">
-                    <div>
-                        <h2>Ready for a recipe?</h2>
-                        <p>Generate a recipe from your list of ingredients.</p>
+            {ingredients.length ? 
+                <section className="ingredient-section">
+                    <h2 className="ingredient-list-title">Ingredients on hand:</h2>
+                    <ul className="ingredients-list" aria-live="polite">{ingredients_list_items}</ul>
+                    <div className="get-recipe-container">
+                        <div>
+                            <h2>Ready for a recipe?</h2>
+                            <p>Generate a recipe from your list of ingredients.</p>
+                        </div>
+                        <button>Get a recipe</button>
                     </div>
-                    <button>Get a recipe</button>
-                </div>
-            </section>
+                </section>
+                : null
+            }
         </main>
     )
 }
