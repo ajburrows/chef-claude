b2eeadc4b067a4b7a41385210f3c22f12847f168
Alex Burrows <burrows.alexj@gmail.com>
2024-11-30 15:48:41 -0500

    Convert the ingredient list to a state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react"
+
 export default function Main(){
+    const [ingredients, setIngredients]= useState([])
+
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const form_data = new FormData(event.currentTarget)
+        const new_ingredient = form_data.get("ingredient")
+        setIngredients(prev_ingredients_list => {
+            return [...prev_ingredients_list, <li key={new_ingredient}>{new_ingredient}</li>]
+        })
+        console.log(ingredients)
+    }
+
     return(
         <main>
-            <form className="add-ingredient-form">
+            <form className="add-ingredient-form" onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredients}
+            </ul>
         </main>
     )
 }
