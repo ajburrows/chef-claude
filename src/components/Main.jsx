33ba18f16c90b109dc73454806ffaa6bb0630e23
Alex Burrows <burrows.alexj@gmail.com>
2024-11-30 19:01:12 -0500

    Only render get-recipe-container if there are 4 or more ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -29,13 +29,16 @@ export default function Main(){
                 <section className="ingredient-section">
                     <h2 className="ingredient-list-title">Ingredients on hand:</h2>
                     <ul className="ingredients-list" aria-live="polite">{ingredients_list_items}</ul>
-                    <div className="get-recipe-container">
-                        <div>
-                            <h2>Ready for a recipe?</h2>
-                            <p>Generate a recipe from your list of ingredients.</p>
+                    {ingredients.length > 3 ?
+                        <div className="get-recipe-container">
+                            <div>
+                                <h2>Ready for a recipe?</h2>
+                                <p>Generate a recipe from your list of ingredients.</p>
+                            </div>
+                            <button>Get a recipe</button>
                         </div>
-                        <button>Get a recipe</button>
-                    </div>
+                        : null
+                    }
                 </section>
                 : null
             }
