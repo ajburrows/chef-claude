6944b1a4f40dae496442810fc1fb707a2a95c4ee
Alex Burrows <burrows.alexj@gmail.com>
2024-11-30 23:11:52 -0500

    Add recipe header and aria-live prop

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -3,10 +3,11 @@ import ReactMarkdown from 'react-markdown'
 
 export default function ClaudeRecipe(props){
     return(
-        <section className='suggested-recipe-container'>
-            <ReactMarkdown>
-                {props.recipeMarkdown}
-            </ReactMarkdown>
-        </section>
+        props.recipeMarkdown ? 
+            <section className='suggested-recipe-container' aria-live='polite'>
+                <h2>Chef Claude Recommends</h2>
+                <ReactMarkdown>{props.recipeMarkdown}</ReactMarkdown>
+            </section>
+            : null
     )
 }
